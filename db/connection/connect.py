43c07defe9383f52a0261ef79b661a5f75104ac0
dryfte <31904625+dryfte@users.noreply.github.com>
2020-02-12 17:15:50 -0500

    added methods two methods: One for connecting to db One for disconnecting from db
    Author: Mitchell Chatterjee

--- db/connection/connect.py
+++ db/connection/connect.py
@@ -3,7 +3,7 @@ import psycopg2
 from config import config
 import os
  
-def connect():
+def testConnection():
     """ Connect to the PostgreSQL database server """
     conn = None
     try:
@@ -33,9 +33,43 @@ def connect():
         if conn is not None:
             conn.close()
             print('Database connection closed.')
-            os.system("PAUSE")
- 
+
+# This method handles connecting to the database
+def connect():
+    conn = None
+    try:
+        # read connection parameters
+        params = config()
+        # connect to the PostgreSQL server
+        print('Connecting to the PostgreSQL database...')
+        conn = psycopg2.connect(**params)
+        # create a cursor
+        cur = conn.cursor()
+        # return a cursor and the connection to the database
+        return (conn, cur)
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+
+# This method handles disconnecting from the database
+# Must be called if the connect method is called upon ending the program
+def disconnect(cur, conn):
+    # terminate the cursor
+    if cur is not None:
+         cur.close()
+    # terminate the database connection itself
+    if conn is not None:
+        conn.close()
+        print('Database connection closed')
+
  
 if __name__ == '__main__':
-    connect()
+    # this the main test method to quickly check if your connection is working
+    testConnection()
+
+    # this is how to actually connect / disconnect fromt the database
+    # print("acutal test")
+    # connection, cursor = connect()
+    # disconnect(connection, cursor)
+    # os.system("PAUSE")
+
     
