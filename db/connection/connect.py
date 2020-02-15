af67fe209c9b6e1e0b934b5fd99cb2eb1991fc72
dryfte <31904625+dryfte@users.noreply.github.com>
2020-02-15 11:01:24 -0500

    fixed the connection class to work as an object.
    Author: Mitchell Chatterjee

--- db/connection/connect.py
+++ db/connection/connect.py
@@ -2,74 +2,59 @@
 import psycopg2
 from config import config
 import os
- 
-def testConnection():
-    """ Connect to the PostgreSQL database server """
-    conn = None
-    try:
-        # read connection parameters
-        params = config()
- 
-        # connect to the PostgreSQL server
-        print('Connecting to the PostgreSQL database...')
-        conn = psycopg2.connect(**params)
-      
-        # create a cursor
-        cur = conn.cursor()
+
+class Database:
+    def __init__(self):
+        self.connection = None
+        try:
+            # read connection parameters
+            params = config()
+            # connect to the PostgreSQL server
+            print('Connecting to the PostgreSQL database...')
+            self.connection = psycopg2.connect(**params)
+            # create a cursor
+            self.cursor = self.connection.cursor()
+        except (Exception, psycopg2.DatabaseError) as error:
+            print(error)
+
+    def __del__(self):
+        self.disconnect()
         
-   # execute a statement
-        print('PostgreSQL database version:')
-        cur.execute('SELECT version()')
- 
-        # display the PostgreSQL database server version
-        db_version = cur.fetchone()
-        print(db_version)
-       
-       # close the communication with the PostgreSQL
-        cur.close()
-    except (Exception, psycopg2.DatabaseError) as error:
-        print(error)
-    finally:
-        if conn is not None:
-            conn.close()
-            print('Database connection closed.')
+    
+    def testConnection(self):
+        """ Connect to the PostgreSQL database server """
+        try:
+            # execute a statement
+            print('PostgreSQL database version:')
+            self.cursor.execute('SELECT version()')
+    
+            # display the PostgreSQL database server version
+            db_version = self.cursor.fetchone()
+            print(db_version)
+        
+            # close the communication with the PostgreSQL
+            self.cursor.close()
+        except (Exception, psycopg2.DatabaseError) as error:
+            print(error)
 
-# This method handles connecting to the database
-def connect():
-    conn = None
-    try:
-        # read connection parameters
-        params = config()
-        # connect to the PostgreSQL server
-        print('Connecting to the PostgreSQL database...')
-        conn = psycopg2.connect(**params)
-        # create a cursor
-        cur = conn.cursor()
-        # return a cursor and the connection to the database
-        return (conn, cur)
-    except (Exception, psycopg2.DatabaseError) as error:
-        print(error)
+    # This method handles disconnecting from the database
+    # Must be called if the connect method is called upon ending the program
+    def disconnect(self):
+        # terminate the cursor
+        if self.cursor is not None:
+            self.cursor.close()
+        # terminate the database connection itself
+        if self.connection is not None:
+            self.connection.close()
+            print('Database connection closed')
 
-# This method handles disconnecting from the database
-# Must be called if the connect method is called upon ending the program
-def disconnect(cur, conn):
-    # terminate the cursor
-    if cur is not None:
-         cur.close()
-    # terminate the database connection itself
-    if conn is not None:
-        conn.close()
-        print('Database connection closed')
-
- 
+    
 if __name__ == '__main__':
     # this the main test method to quickly check if your connection is working
-    testConnection()
+    # to use this class just remove the conn.testConnection() method
+    conn = Database()
+    conn.testConnection()
+    del conn
 
-    # this is how to actually connect / disconnect fromt the database
-    # print("acutal test")
-    # connection, cursor = connect()
-    # disconnect(connection, cursor)
-    # os.system("PAUSE")
-
+    os.system("PAUSE")
     
